72e80d62e0e1c070eef8c935fa52b0bdbdcb17ec
Jesus Santos Lopez <jesus.santoslopez@altran.com>
2018-11-24 16:47:48 +0100

    Init M3 task

---
+++ pluralsight-springcloud-m3-task/src/main/java/pluralsight/demo/PluralsightSpringcloudM3TaskApplication.java
@@ -0,0 +1,12 @@
+package pluralsight.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluralsightSpringcloudM3TaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluralsightSpringcloudM3TaskApplication.class, args);
+	}
+}
