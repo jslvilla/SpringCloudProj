e33dfc82dd27b19d8fd8a318bc59756e86cee9c1
Jesus Santos Lopez <jesus.santoslopez@altran.com>
2018-11-30 22:39:12 +0100

    M4 02 Init EnableResourceServer

---
+++ pluralsight-springcloud-m4-secureservice/src/main/java/pluralsight/demo/PluralsightSpringcloudM4SecureserviceApplication.java
@@ -0,0 +1,12 @@
+package pluralsight.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluralsightSpringcloudM4SecureserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluralsightSpringcloudM4SecureserviceApplication.class, args);
+	}
+}
