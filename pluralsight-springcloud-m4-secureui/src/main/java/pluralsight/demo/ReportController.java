7182846f1cfd0f14a677fb09dc56ed9a99340bfd
Jesus Santos Lopez <jesus.santoslopez@altran.com>
2018-11-30 22:26:17 +0100

    M4 1 01 Init GitHub Authentication

--- pluralsight-springcloud-m4-secureui/src/main/java/pluralsight/demo/ReportController.java
+++ pluralsight-springcloud-m4-secureui/src/main/java/pluralsight/demo/ReportController.java
@@ -1,28 +1,15 @@
 package pluralsight.demo;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @EnableOAuth2Sso
-public class ReportController extends WebSecurityConfigurerAdapter {
+public class ReportController {
 	
 	@RequestMapping("/")
 	public String loadHome() {
 		return "home";
 	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-		.authorizeRequests()
-			.antMatchers("/", "/login**")
-			.permitAll()
-		.anyRequest()
-			.authenticated();
-	}
-
 }
