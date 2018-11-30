b1b46148b74796fd1c2ddb0f638ec7437c804d81
Jesus Santos Lopez <jesus.santoslopez@altran.com>
2018-11-30 17:57:19 +0100

    M3 - 3 Not totally OK - Exception when Postman send the message

--- pluralsight-springcloud-m3-taskintake/src/test/java/pluralsight/demo/PluralsightSpringcloudM3TaskintakeApplicationTests.java
+++ pluralsight-springcloud-m3-taskintake/src/test/java/pluralsight/demo/PluralsightSpringcloudM3TaskintakeApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PluralsightSpringcloudM3TaskApplicationTests {
+public class PluralsightSpringcloudM3TaskintakeApplicationTests {
 
 	@Test
 	public void contextLoads() {
