e809bce52fa854138783c2709ae3574e569f240d
Jesus Santos Lopez <jesus.santoslopez@altran.com>
2018-12-02 00:28:27 +0100

    M4 02 04 Web OK - Authentication, seeing correctly the report in  html

--- pluralsight-springcloud-m4-secureui/src/main/java/pluralsight/demo/ReportController.java
+++ pluralsight-springcloud-m4-secureui/src/main/java/pluralsight/demo/ReportController.java
@@ -1,10 +1,16 @@
 package pluralsight.demo;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +22,9 @@ public class ReportController extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private OAuth2ClientContext clientContext;
+	
+	@Autowired
+	private OAuth2RestTemplate oauth2RestTemplate;
 	
 	@RequestMapping("/")
 	public String loadHome() {
@@ -29,7 +38,28 @@ public class ReportController extends WebSecurityConfigurerAdapter {
 		OAuth2AccessToken t = clientContext.getAccessToken();
 		System.out.println("Token: " + t.getValue());
 		
+		ResponseEntity<ArrayList<TollUsage>> tolls = oauth2RestTemplate.exchange("http://localhost:9001/services/tolldata", HttpMethod.GET, null, new ParameterizedTypeReference<ArrayList<TollUsage>>(){});
+		
+		model.addAttribute("tolls", tolls.getBody());
+		
 		return "reports";
+	}
+
+
+	public static class TollUsage {		
+		public String Id;
+		public String stationId;
+		public String licenseplate;
+		public String timestamp;
+		
+		public TollUsage() {}
+		
+		public TollUsage(String id, String stationid, String licenseplate, String timestamp){
+			this.Id = id;
+			this.stationId = stationid;
+			this.licenseplate = licenseplate;
+			this.timestamp = timestamp;
+		}		
 	}
 	
 	@Override
