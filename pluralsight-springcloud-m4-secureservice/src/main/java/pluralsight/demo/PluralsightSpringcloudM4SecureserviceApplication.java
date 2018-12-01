07d650ee53fb453b1963ffb8283285028e4be3bb
Jesus Santos Lopez <jesus.santoslopez@altran.com>
2018-12-01 23:36:02 +0100

    M4 02 02 @EnableResourceServer not failing - Postman Fail -> unauthorized - Full authentication is required to access this resource

--- pluralsight-springcloud-m4-secureservice/src/main/java/pluralsight/demo/PluralsightSpringcloudM4SecureserviceApplication.java
+++ pluralsight-springcloud-m4-secureservice/src/main/java/pluralsight/demo/PluralsightSpringcloudM4SecureserviceApplication.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableResourceServer
 public class PluralsightSpringcloudM4SecureserviceApplication {
 
 	public static void main(String[] args) {
