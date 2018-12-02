d99211104f617e73fa545209bfc0bdfe019d4db9
Jesus Santos Lopez <jesus.santoslopez@altran.com>
2018-12-02 14:01:44 +0100

    M4 03 01 Init Secure Auth Server

---
+++ pluralsight-springcloud-m4-secureauthserver/src/main/java/pluralsight/demo/PluralsightSpringcloudM4SecureauthserverApplication.java
@@ -0,0 +1,12 @@
+package pluralsight.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluralsightSpringcloudM4SecureauthserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluralsightSpringcloudM4SecureauthserverApplication.class, args);
+	}
+}
