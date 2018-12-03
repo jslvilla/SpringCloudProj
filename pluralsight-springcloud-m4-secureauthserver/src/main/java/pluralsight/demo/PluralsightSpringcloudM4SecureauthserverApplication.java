f242cdcfa377538714283933d21f523164b01289
Jesus Santos Lopez <jesus.santoslopez@altran.com>
2018-12-03 21:19:59 +0100

    M4 03 02 Auth Server

--- pluralsight-springcloud-m4-secureauthserver/src/main/java/pluralsight/demo/PluralsightSpringcloudM4SecureauthserverApplication.java
+++ pluralsight-springcloud-m4-secureauthserver/src/main/java/pluralsight/demo/PluralsightSpringcloudM4SecureauthserverApplication.java
@@ -2,8 +2,10 @@ package pluralsight.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class PluralsightSpringcloudM4SecureauthserverApplication {
 
 	public static void main(String[] args) {
