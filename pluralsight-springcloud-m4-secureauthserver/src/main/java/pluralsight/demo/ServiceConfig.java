2ba7355b7f7e0e54b831c68f98340ed0ca1fddd7
Jesus Santos Lopez <jesus.santoslopez@altran.com>
2018-12-04 23:38:32 +0100

    M4 03 03 Service Config OK

--- pluralsight-springcloud-m4-secureauthserver/src/main/java/pluralsight/demo/ServiceConfig.java
+++ pluralsight-springcloud-m4-secureauthserver/src/main/java/pluralsight/demo/ServiceConfig.java
@@ -1,17 +1,38 @@
 package pluralsight.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ServiceConfig extends GlobalAuthenticationConfigurerAdapter{
-
+		
+		@Bean
+		public PasswordEncoder passwordEncoder() {
+		    String idForEncode = "bcrypt";
+		    Map<String, PasswordEncoder> encoderMap = new HashMap<>();
+		    encoderMap.put(idForEncode, new BCryptPasswordEncoder());
+		    return new DelegatingPasswordEncoder(idForEncode, encoderMap);
+		}
+	
+		@Autowired
+		private PasswordEncoder passcoder;
+	
 		@Override
 		public void init(AuthenticationManagerBuilder auth) throws Exception {
+			//passcoder.encode("pluralsight");
+			
 			auth.inMemoryAuthentication()
-			.withUser("agoldberg").password("pass1").roles("USER").and()
-			.withUser("bgoldberg").password("pass2").roles("USER","OPERATOR");
+			.withUser("agoldberg").password(passcoder.encode("pass1")).roles("USER").and()
+			.withUser("bgoldberg").password(passcoder.encode("pass2")).roles("USER","OPERATOR");
 		}
 }
 
